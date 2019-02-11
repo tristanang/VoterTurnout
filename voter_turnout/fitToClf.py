8f7f91b4a18f1d4be0d75f6a894184314fadeda7
Sophia Coplin <scoplin@caltech.edu>
2019-02-11 00:44:53 -0800

    Working simple Forest classifier

---
+++ voter_turnout/fitToClf.py
@@ -0,0 +1,44 @@
+
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Custom
+import normalize
+
+# Path with model info
+path = "forest, 100, 20/"
+
+# Import scaler, classifier
+file = open( path + "scaler.pickle", "rb" )
+scaler = pickle.load(file)
+file.close
+
+file = open( path + "clf.pickle", "rb" )
+clf = pickle.load(file)
+file.close
+
+
+# Import input set
+file = open( "../data/test_input.pickle", "rb" )
+X = pickle.load(file)
+file.close
+
+
+# Normalize
+normalize.scale(X, scaler, True)
+
+# Predict
+y_predict = clf.predict(X)
+
+# Output format
+arr = pd.DataFrame(y_predict)
+arr.columns = ["target"]
+
+print(arr)
+
+# Export
+arr.to_csv("predicted_target.csv")
--- voter_turnout/tree.py
+++ voter_turnout/tree.py
@@ -1,12 +1,13 @@
 # Code from set 3
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-clf = tree.DecisionTreeClassifier(max_depth = 20, criterion='gini')
+clf = RandomForestClassifier(n_estimators = 100, max_depth = 20, criterion='gini')
 
 clf.fit(X_train, y_train)
 
-train_err = classification_err(clf.predict(X_train), y_train)
-test_err = classification_err(clf.predict(X_val), y_val)
+train_acc = accuracy_score(clf.predict(X_train), y_train)
+val_acc = accuracy_score(clf.predict(X_val), y_val)
 
-print("train error: ", train_error, "test error: ", test_err)
+print("train error: ", train_acc, "test error: ", val_acc)
