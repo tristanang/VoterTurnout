a3b479ee2e11abf6f5f357d21dcc7d7ab35a67fe
Sophia Coplin <scoplin@caltech.edu>
2019-02-11 11:04:09 -0800

    Add neural net (mainNeural) and move PCA/norm out of main

--- voter_turnout/PCA.py
+++ voter_turnout/PCA.py
@@ -1,20 +1,57 @@
-# PCA
+# Do normalization and PCA
 # From https://www.kaggle.com/pmmilewski/pca-decomposition-and-keras-neural-network
 
-pca = PCA(n_components=1000)
-pca.fit(X_train)
+import pickle
+from sklearn.decomposition import PCA
 
-# Visualize how many components needed
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.xlabel('Number of components')
-plt.ylabel('Cumulative explained variance')
+from voter_turnout import normalize
+from voter_turnout.preprocess import gradient_maps 
 
-plt.show()
+# Load data
+save_path = "data/pca"
+name = "data/train_input"
+file = open( '{}.pickle'.format(name), "rb" )
+X = pickle.load(file)
+file.close
 
-# Re-run PCA for components wanted
-NCOMPONENTS = 100
+# Normalization
+colsToScale = gradient_maps.toNormalize
+# Use this for all normalizations
+scaler = normalize.retScaler(X, colsToScale)
+normalize.scale(X, scaler, True)
 
-pca = PCA(n_components=NCOMPONENTS)
-X_train = pca.fit_transform(X_train)
-#X_val = pca.transform(X_val)
-pca_std = np.std(X_pca_train)
+# Save scaler
+file = open(save_path + "scaler.pickle", 'wb')
+pickle.dump(scaler, file)
+file.close()
+
+# Do PCA
+for n_components in [50, 75, 100, 200, 500, 1000]:
+    
+    
+    pca = PCA(n_components=n_components)
+    X_transf = pca.fit_transform(X)
+
+    # Save PCA
+    print('{}/pca_{}.pickle'.format(save_path, n_components))
+    file = open('{}/pca_{}.pickle'.format(save_path, n_components), 'wb')
+    pickle.dump(scaler, file)
+    file.close()  
+    
+    # Save transformed data
+    file = open('{}_norm_PCA{}.pickle'.format(name, n_components), 'wb')    
+    pickle.dump(X_transf, file)
+    file.close()
+    
+
+
+def chooseN_components():
+    pca = PCA(n_components=1000)
+    pca.fit(X_train)
+    
+    # Visualize how many components needed
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('Number of components')
+    plt.ylabel('Cumulative explained variance')
+    
+    plt.show()    
--- voter_turnout/neuralNet.py
+++ voter_turnout/neuralNet.py
@@ -6,6 +6,41 @@ import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 
+def neuralNet(X, y, X_val = None, y_val = None):
+    ## Create your own model here given the constraints in the problem
+    model = Sequential()
+    
+    # Add layers
+    model.add(Dense(200))
+    model.add(Activation('relu'))
+    
+    model.add(Dense(100))
+    model.add(Activation('relu'))   
+    
+    # Add a sigmoid layer to output probability
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    
+    
+    # Binary classification so use binary_crossentropy
+    model.compile(loss='binary_crossentropy',
+                  optimizer='RMSprop', 
+                  metrics=['accuracy'])
+    
+    # Defaults. Found little benefit of changing.
+    batch_size = 32
+    epochs = 10
+    if X_val is not None and y_val is not None:
+        model.fit(X, y, batch_size=batch_size, epochs=epochs,
+                  verbose=1, 
+                  validation_data = (X_val, y_val))
+    else:
+        model.fit(X, y, batch_size=batch_size, epochs=epochs,
+                  verbose=1)
+        
+    return model
+
+'''
 ## Create your own model here given the constraints in the problem
 model = Sequential()
 
@@ -17,6 +52,8 @@ model.add(Dropout(0.002))
 ## Add a sigmoid layer to output probability
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
+
+
 
 # One-hot encoding the labels, so use 'categorical_crossentropy' as loss.
 ## You will likely have the best results with RMS prop or Adam as your optimizer.  In the line below we use Adadelta
@@ -31,9 +68,6 @@ epochs = 10
 fit = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
     verbose=1)
 	
-## Problem D
-## Printing a summary of the layers and weights in your model
-model.summary()
 
 ## Printing the accuracy of our model, according to the loss function specified in model.compile above
 score = model.evaluate(X_test, y_test, verbose=0)
@@ -41,3 +75,4 @@ in_sample_score = model.evaluate(X_train, y_train, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
 print('In-sample accuracy:', in_sample_score[1])
+'''
